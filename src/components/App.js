814d856fc6cf90a75f446538c6b8db3302ae90be
AadarshSu <as566@st-andrews.ac.uk>
2022-12-31 11:52:00 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  // contacts array manipulated by addNewContacts method
+  const [contacts, addNewContacts] = useState(
+    !localStorage.getItem(LOCAL_STORAGE_KEY)
+      ? []
+      : JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+  );
+  // get contacts and add them from the AddContact component using handler
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    addNewContacts([...contacts, { id: uuid(), ...contact }]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    addNewContacts(newContactList);
+  }
+
+  // gets contacts from local storage everytime page is refreshed
+  useEffect(() => {
+    const getContactsFromLocalStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    addNewContacts(getContactsFromLocalStorage);
+  }, []);
+
+  // store contacts in local storage so they don't disappear on refresh
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts} getContactID={removeContactHandler}/>
+    </div>
+  );
+}
+
+export default App;
